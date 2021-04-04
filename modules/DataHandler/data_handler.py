81f8f76b85894f78cec8729682c1300cbd5a4f9c
balkon16 <pawel.k.lonca@gmail.com>
2021-04-04 13:25:35 +0200

    a stub for DataHandler class

---
+++ modules/DataHandler/data_handler.py
@@ -0,0 +1,11 @@
+"""
+The class implements a tool for data processing: getting selected fields, computing delta, handling various data types.
+"""
+
+# TODO: entity<->fields mapping
+# TODO: (?) computing delta
+
+class DataHandler:
+
+    def __init__(self):
+        pass
